98be433476b91b3778be0fb04d43528c65d16140
Claudia Chittim <cchittim10101@gmail.com>
2022-06-03 19:35:02 -0500

    added line 4 so server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const db = require("./config/connection");
 const routes = require("./routes");
+const mongoose = require("mongoose");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,8 +10,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
-db.once("open", () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-  });
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/social_network",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+// Use this to log mongo queries being executed
+mongoose.set("debug", true);
+
+// Use this to log mongo queries being executed!
+mongoose.set("debug", true);
+
+app.use(require("./routes"));
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
