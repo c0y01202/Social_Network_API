e9d8a13899d4b09eb180e09cb724cbd1f5dafb8f
Claudia Chittim <cchittim10101@gmail.com>
2022-06-03 21:03:41 -0500

    tried to connect to server and changed server.js file again

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(routes);
+app.use(require("./routes"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/social_network_API",
+  process.env.MONGODB_URI || "mongodb://localhost:27017/socialNetwork",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -19,10 +19,5 @@ mongoose.connect(
 
 // Use this to log mongo queries being executed
 mongoose.set("debug", true);
-
-// Use this to log mongo queries being executed!
-mongoose.set("debug", true);
-
-app.use(require("./routes"));
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
