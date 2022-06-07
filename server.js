6f2b28616edcf9a09daf6537117d1acd4754fb87
Claudia Chittim <cchittim10101@gmail.com>
2022-06-07 16:27:48 -0500

    trying to connect to server

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(require("./routes"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/socialNetwork",
+  process.env.MONGODB_URI || "mongodb://localhost:27017/social_network_api",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -20,4 +20,6 @@ mongoose.connect(
 // Use this to log mongo queries being executed
 mongoose.set("debug", true);
 
+//run connect here and then a .then
+//nest the app.listen inside of this
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
